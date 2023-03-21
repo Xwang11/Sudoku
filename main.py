07ed6bef9875fe63da9053779cda3bfca5b436aa
Xiaotian <58612496+Xwang11@users.noreply.github.com>
2023-03-21 18:06:13 -0400

    Debugged. Error lies in the incrementation when there is not a blank. Next error is that values repeat in squares

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from boardNew import Board
 from solver import Solver
+import time
 b = Board()
 s = Solver()
 
@@ -14,6 +15,16 @@ m = [[9,2,0,0,0,5,8,0,0],
      [0,4,0,6,0,7,9,1,5],
      [0,0,0,2,0,0,6,0,0]
      ]
+# m = [[5,2,7,6,1,3,4,8,9],
+#      [1,8,6,9,7,4,2,3,5],
+#      [4,9,3,5,8,2,6,7,1],
+#      [2,7,1,4,9,6,8,5,3],
+#      [9,6,5,2,3,8,7,1,4],
+#      [3,4,8,1,5,7,9,6,2],
+#      [6,1,2,7,4,5,3,9,8],
+#      [8,5,4,3,6,9,1,2,7],
+#      [7,3,9,8,2,1,5,4,0]
+#      ]
 
 # Evil lv problem - cannot solve brute force method
 n = [[0,2,0,0,1,0,0,8,0],
@@ -26,10 +37,13 @@ n = [[0,2,0,0,1,0,0,8,0],
      [8,0,0,0,0,9,0,0,0],
      [0,0,9,0,2,0,5,0,6]]
 
-b.setBoard(m)
-
+start = time.perf_counter()
+b.setBoard(n)
+print(s.roundBox(8))
 s.solve(b, 0, 0)
-
+end = time.perf_counter()
+print(end-start)
+# b.printBoard()
 
 
 
--- solver.py
+++ solver.py
@@ -33,7 +33,7 @@ class Solver:
             if (b[x][j] == k):
                 return False
         
-        # Check box
+        # Check box BROKEN
         for y in range(self.roundBox(i), self.roundBox(i) + 2):
             for x in range(self.roundBox(j), self.roundBox(j)+2):
                 if b[y][x] == k:
@@ -43,40 +43,41 @@ class Solver:
     
     
     def solve(self,board, i, j):
-        board.printBoard()
+        # board.printBoard()
         b=board.getBoard()
-        print(f"Now checking ({i}, {j}) that has value {b[i][j]}")
+       
 
-        if (i == 8) and (j == 8) and (b[i][j] != 0):
+        if i >= 9:
             print("Complete")
+            # board.printBoard()
             return True
         
         elif b[i][j] == 0:
+            # print(f"Now checking ({i}, {j}) that has value {b[i][j]}")
             for k in range(1,10):
                 if self.checkValid(board, i, j, k):
-                    print(f"({i}, {j}) can be solved with {k}")   
+                    # print(f"({i}, {j}) can be solved with {k}")   
                     b[i][j] = k
                     board.setBoard(b)
                     if i==8 and j==8:
                         return True
-                    if i==8:
-                        jNew=j+1
-                        iNew=0
+                    if j==8:
+                        iNew=i+1
+                        jNew=0
                     else:
-                        iNew = i+1
-                        jNew = j
+                        jNew = j + 1
+                        iNew = i
                     
-                    if self.solve(board,iNew,jNew) == False:
-                        b[i][j] = 0
+                    if self.solve(board,iNew,jNew):
+                        return True
+                    b[i][j] = 0
             return False
 
         else:
-            print(f"({i}, {j}) is not a blank space, incrementing")   
-            if i==8:
-                j=j+1
-                i=0
-            else:
+            # print(f"({i}, {j}) is not a blank space, incrementing")   
+            if j==8:
                 i=i+1
-            if self.solve(board,i,j):
-                        return self.solve(board,i,j)
-            return False
+                j=0
+            else:
+                j=j+1
+            return self.solve(board,i,j)
